6a08a753fa419a92355c270fe7ada96c19b7c9f2
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2020-01-26 12:41:26 -0500

    scrape articles from infoworld into my mongodb database

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/newsletter", { useNewUrlParser: true });
+
+app.get("/scrape",(req, res) => {
+    axios.get("https://www.infoworld.com/category/javascript/").then(function (response) {
+        const $ = cheerio.load(response.data);
+        
+        $(".article").each((i, element) => {
+            const result = {};
+            result.title = $(element)
+            .find("h3")
+            .text();
+            result.summary = $(element)
+            .find("h4")
+            .text();
+            result.link = `https://www.infoworld.com/${$(this)
+            .find("a")
+            .attr("href")}`;
+
+            // console.log(result); 
+            db.Article.create(result)
+            .then(dbArticle => {
+                console.log(dbArticle);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        })
+        
+
+        res.send("Scrape Complete");
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log("App running on port " + PORT + "!");
+});
