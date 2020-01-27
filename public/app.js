137150262030be3eb79d7b8c7d24410fa4ddc1d8
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2020-01-26 23:33:58 -0500

    make articles collapse on click

--- public/app.js
+++ public/app.js
@@ -10,18 +10,39 @@ $(document).ready(() => {
             }).then((result) => {
                 // console.log(result);
                 $.getJSON("/articles", data => {
-                    const articles = $("#articles");
-                    data.forEach(data => {
-                            articles.append(`
-                            ${data.title}<br>
-                            ${data.summary}<br>
-                            <a href='https://www.infoworld.com${data.link}'>Article Link</a><br><br>
-                            `);
-                    });
+                    const articles = $(".accordion");
+                    const title = data.map((data) => {
+                        return data.title.trim();
+                    })
+                    const summary = data.map(data => {
+                        return data.summary.trim();
+                    })
+                    const link = data.map(data => {
+                        return data.link;
+                    })
+                    for (let i = 0; i < data.length; i++) {
+                        // data.forEach(data => {
+                        articles.append(`
+                        <div class="card-header" id="heading${i}>
+                            <h2 class="mb-0">
+                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">${title[i]}</button>
+                            </h2>
+                        </div>
+                        <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+                            <div class="card-body">
+                                ${summary[i]}<br>
+                                <a href='https://www.infoworld.com${link[i]}'>Article Link</a><br><br>
+                            </div>
+                        </div>
+                        `);
+                    }
                 });
             });
         });
     });
+
+
+
 
     //save note button
     $(document).on("submit", "#saveNote", (event) => {
