3ab069d87161c054821d941f4eadce4f081c057a
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2020-01-27 00:54:31 -0500

    get the id for the add-notes button

--- public/app.js
+++ public/app.js
@@ -11,43 +11,40 @@ $(document).ready(() => {
                 // console.log(result);
                 $.getJSON("/articles", data => {
                     const articles = $(".accordion");
-                    const title = data.map((data) => {
-                        return data.title.trim();
-                    })
-                    const summary = data.map(data => {
-                        return data.summary.trim();
-                    })
-                    const link = data.map(data => {
-                        return data.link;
-                    })
+                    console.log(data);
                     for (let i = 0; i < data.length; i++) {
-                        // data.forEach(data => {
                         articles.append(`
                         <div class="card-header" id="heading${i}>
                             <h2 class="mb-0">
-                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">${title[i]}</button>
+                                <button class="btn btn-link titleCSS" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">${data[i].title.trim()}</button>
                             </h2>
                         </div>
                         <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
                             <div class="card-body">
-                                ${summary[i]}<br>
-                                <a href='https://www.infoworld.com${link[i]}'>Article Link</a><br><br>
+                                ${data[i].summary.trim()}<br><br>
+                                <a href='https://www.infoworld.com${data[i].link}'>Article Link</a>
+                                <button data-id=${data[i]._id} class="add-notes">Notes</button>
                             </div>
                         </div>
                         `);
                     }
                 });
             });
-        });
+        });   
     });
 
+    $(document).on("click", `.add-notes`, (event) => {
+        const thisID = $(event.target)[0].dataset.id;
+        console.log(event.target.dataset.id);
+        console.log(thisID);
+    });
 
 
 
     //save note button
-    $(document).on("submit", "#saveNote", (event) => {
-        event.preventDefault()
-        console.log('hi');
-    });
+    // $(document).on("submit", "#saveNote", (event) => {
+    //     event.preventDefault()
+    //     console.log('hi');
+    // });
 
 });
