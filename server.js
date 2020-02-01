1e08d5800920f2b8e97e5d1d39e3c124cdd951d6
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2020-01-31 23:09:01 -0500

    add process.env to PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cheerio = require("cheerio");
 
 const db = require("./models");
 
-const PORT = 3000;
+const PORT = process.env.MONGODB_URI || 3000 ;
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds129098.mlab.com:29098/heroku_hxdh4c6z";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds129098.mlab.com:29098/heroku_hxdh4c6z";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connect(MONGODB_URI, { useFindAndModify: false });
 
@@ -44,7 +44,7 @@ app.get("/scrape", (req, res) => {
                     err => res.send(err)
                 );
             }
-            
+
         });
         res.send("Scrape Complete");
     }
